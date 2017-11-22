e7dec6d2e9e32466801fa1eed6a4429ae2ad48c9
Jason Ruby <jaruby@me.com>
2017-11-22 11:50:03 -0600

    Remove superseded code from drum-kit.js

--- drum-kit.js
+++ drum-kit.js
@@ -1,6 +1,5 @@
 window.addEventListener('keydown', function(event) {
-	// console.log(event.keyCode);
 	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-	console.log(audio);
-
+	if (!audio) return;
+	audio.play();
 });
